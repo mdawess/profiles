ce2a9f5ddbcf729d495dfc8c36e9db678b075453
mdawess <michaelanthony.dawes@mail.utornto.ca>
2022-08-19 19:48:38 -0600

    more server fixes

--- backend/pull_data.py
+++ backend/pull_data.py
@@ -62,8 +62,12 @@ def make_competitor_dictionary() -> dict:
         elif response[1] not in competitor_dict and response[2] == 'Yes':
             competitor_dict[response[1]] = {
                 'skills': [int(response[17]), int(response[18]), int(response[19]), int(response[20])],
-                'workingStyle': response[21].split(','),
+                
             }
+            try:
+                competitor_dict[response[1]]['workingStyle'] = response[21].split(',')
+            except:
+                pass
         elif response[1] in competitor_dict and response[2] == 'Yes':
             competitor_dict[response[1]]['skills'] = [mean(i) for i in zip(competitor_dict[response[1]]['skills'],[int(response[17]), int(response[18]), int(response[19]), int(response[20])])]
             try:
